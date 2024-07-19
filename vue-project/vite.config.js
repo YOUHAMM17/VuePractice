c53e6dde27951cfebb28a4ce7daf7193cb61e6a8
youham17 <ec091334@ens.uqam.ca>
2024-07-19 15:21:20 -0400

    mise a jour

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -2,11 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { writeFileSync } from 'fs'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    {
+      name: 'generate-redirect',
+      closeBundle() {
+        writeFileSync('./dist/_redirects', '/* /index.html 200')
+      }
+    }
   ],
   resolve: {
     alias: {
